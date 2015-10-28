9597e5841eaf3e6d0cd1eda1fb886f80c14dd287
Jacqueline Fedyk <jafed100@pc05.pool.tp1.uni-duesseldorf.de>
2015-10-28 11:38:42 +0100

    Fibonacci numbers

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,6 +1,16 @@
-#include <iostream>
 
-int main(){
-  std::cout << "hallo" << std::endl;
-  return 0;
-}
+#include <iostream>
+using namespace std;
+
+int main(){ 
+  
+   int f0=0, f1=1, f;
+    
+    for(int i=0; i<11; i++)
+       {cout << "f0=" << f0 << endl;
+        f=f1+f0;
+        f0=f1;
+        f1=f;
+       }
+  return 0;}
+
